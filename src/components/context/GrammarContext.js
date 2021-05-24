ce4457cbbacfb8ab6cb8d788c7c9306ded05c66b
AnuMonachan <anumonachan9953@gmail.com>
2021-05-24 20:08:05 +0530

    Added Default USFM Text

--- src/components/context/GrammarContext.js
+++ src/components/context/GrammarContext.js
@@ -5,7 +5,20 @@ export const GrammarContext = createContext();
 
 const GrammarContextProvider = (props) => {
   const [jsonValue, setJsonValue] = useState("");
-  const [usfmValue, setUsfmValue] = useState("");
+  const [usfmValue, setUsfmValue] = useState(
+    `\\id hab 45HABGNT92.usfm, Good News Translation, June 2003
+\\c 3
+\\s1 A Prayer of Habakkuk
+\\p
+\\v 1 This is a prayer of the prophet Habakkuk:
+\\b
+\\q1
+\\v 2 O \\nd Lord\\nd*, I have heard of what you have done,
+\\q2 and I am filled with awe.
+\\q1 Now do again in our times
+\\q2 the great deeds you used to do.
+\\q1 Be merciful, even when you are angry.`
+  );
   const [csvValue, setCsvValue] = useState("");
   const [tsvValue, setTsvValue] = useState("");
   const [tabValue, setTabValue] = useState(0);
