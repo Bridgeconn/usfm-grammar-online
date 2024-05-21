0845f0c1821a12dec45d80559108e6b81d0e3584
bobbykumar706584 <bobbykumar706584@gmail.com>
2024-05-21 17:35:13 +0530

    css fixed

--- web-app-v3/src/IncludeExcludeFilters.jsx
+++ web-app-v3/src/IncludeExcludeFilters.jsx
@@ -1,57 +1,60 @@
+/** @format */
+
 import { useState } from "react";
 import Select from "react-select";
 
 const customStyles = {
-  control: (provided, state) => ({
-    ...provided,
-    borderRadius: "8px", // Set border radius as desired
-  }),
+	control: (provided, state) => ({
+		...provided,
+		borderRadius: "8px", // Set border radius as desired
+	}),
 };
 const options = [
-  { value: "", label: "All" },
-  { value: "BOOK_HEADERS", label: "BOOK_HEADERS" },
-  { value: "TITLES", label: "TITLES" },
-  { value: "COMMENTS", label: "COMMENTS" },
-  { value: "PARAGRAPHS", label: "PARAGRAPHS" },
-  { value: "CHARACTERS", label: "CHARACTERS" },
-  { value: "NOTES", label: "NOTES" },
-  { value: "STUDY_BIBLE", label: "STUDY_BIBLE" },
-  { value: "BCV", label: "BCV" },
-  { value: "TEXT", label: "TEXT" },
+	{ value: "", label: "All" },
+	{ value: "BOOK_HEADERS", label: "BOOK_HEADERS" },
+	{ value: "TITLES", label: "TITLES" },
+	{ value: "COMMENTS", label: "COMMENTS" },
+	{ value: "PARAGRAPHS", label: "PARAGRAPHS" },
+	{ value: "CHARACTERS", label: "CHARACTERS" },
+	{ value: "NOTES", label: "NOTES" },
+	{ value: "STUDY_BIBLE", label: "STUDY_BIBLE" },
+	{ value: "BCV", label: "BCV" },
+	{ value: "TEXT", label: "TEXT" },
 ];
 
 export default function IncludeExcludeFilter({ onChange }) {
-  const [selectedOptions, setSelectedOptions] = useState([options[0]]); // "All" as default selected value
+	const [selectedOptions, setSelectedOptions] = useState([options[0]]); // "All" as default selected value
 
-  const handleChange = (selectedOption) => {
-    const lastSelectedOption = selectedOption[selectedOption.length - 1];
-    if (lastSelectedOption && lastSelectedOption.value === "") {
-      // If "All" option is selected, set all options except "All" as selected
-      setSelectedOptions(options.slice(0, 1)); // Select only "All"
-      onChange(options.slice(0, 1));
-    } else {
-      // If any other option is selected, remove "All" option from selected list
-      setSelectedOptions(
-        selectedOption.filter((option) => option.value !== "")
-      );
-      onChange(selectedOption.filter((option) => option.value !== ""));
-    }
-  };
+	const handleChange = (selectedOption) => {
+		const lastSelectedOption = selectedOption[selectedOption.length - 1];
+		if (lastSelectedOption && lastSelectedOption.value === "") {
+			// If "All" option is selected, set all options except "All" as selected
+			setSelectedOptions(options.slice(0, 1)); // Select only "All"
+			onChange(options.slice(0, 1));
+		} else {
+			// If any other option is selected, remove "All" option from selected list
+			setSelectedOptions(
+				selectedOption.filter((option) => option.value !== "")
+			);
+			onChange(selectedOption.filter((option) => option.value !== ""));
+		}
+	};
 
-  // Include the "All" option in filtered options if it's not selected
-  const filteredOptions = selectedOptions.some((option) => option.value === "")
-    ? options.slice(1)
-    : options;
+	// Include the "All" option in filtered options if it's not selected
+	const filteredOptions = selectedOptions.some((option) => option.value === "")
+		? options.slice(1)
+		: options;
 
-  return (
-    <div className="ml-2">
-      <Select
-        value={selectedOptions}
-        onChange={handleChange}
-        options={filteredOptions}
-        isMulti
-        styles={customStyles}
-      />
-    </div>
-  );
+	return (
+		<div className="ml-4 mt-0.5">
+			<Select
+				value={selectedOptions}
+				onChange={handleChange}
+				options={filteredOptions}
+				isMulti
+				styles={customStyles}
+				width={200}
+			/>
+		</div>
+	);
 }
--- web-app-v3/src/LeftSourceSelect.jsx
+++ web-app-v3/src/LeftSourceSelect.jsx
@@ -1,78 +1,76 @@
+/** @format */
+
 import { Fragment, useEffect, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
 const fileFormats = [
-  { name: "USFM", unavailable: false },
-  { name: "USJ", unavailable: true },
-  { name: "USX", unavailable: true },
+	{ name: "USFM", unavailable: false },
+	{ name: "USJ", unavailable: true },
+	{ name: "USX", unavailable: true },
 ];
 
 export default function LeftSourceSelect({ onChange }) {
-  const [selected, setSelected] = useState("");
-  useEffect(() => {
-    setSelected(fileFormats[0]);
-  }, []);
+	const [selected, setSelected] = useState("");
+	useEffect(() => {
+		setSelected(fileFormats[0]);
+	}, []);
 
-  return (
-    <div className=" w-24 md:w-28 xl:w-48">
-      <Listbox
-        value={selected}
-        onChange={(newValue) => {
-          setSelected(newValue);
-          onChange(newValue);
-        }}
-      >
-        <div className="relative mt-1">
-          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">{selected.name}</span>
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronUpDownIcon
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </span>
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-28 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-              {fileFormats.map((fileFormat, ffIdx) => (
-                <Listbox.Option
-                  key={ffIdx}
-                  className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? "bg-amber-100 text-amber-900" : "text-gray-900"
-                    }`
-                  }
-                  value={fileFormat}
-                  disabled={fileFormat.unavailable}
-                >
-                  {({ selected }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? "font-medium" : "font-normal"
-                        } ${fileFormat.unavailable ? `text-gray-500` : ""}`}
-                      >
-                        {fileFormat.name}
-                      </span>
-                      {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600 ">
-                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox>
-    </div>
-  );
+	return (
+		<div className="w-36 ml-2 z-50">
+			<Listbox
+				value={selected}
+				onChange={(newValue) => {
+					setSelected(newValue);
+					onChange(newValue);
+				}}>
+				<div className="relative mt-1">
+					<Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+						<span className="block truncate">{selected.name}</span>
+						<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+							<ChevronUpDownIcon
+								className="h-5 w-5 text-gray-400"
+								aria-hidden="true"
+							/>
+						</span>
+					</Listbox.Button>
+					<Transition
+						as={Fragment}
+						leave="transition ease-in duration-100"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0">
+						<Listbox.Options className="absolute mt-1 max-h-60 w-28 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+							{fileFormats.map((fileFormat, ffIdx) => (
+								<Listbox.Option
+									key={ffIdx}
+									className={({ active }) =>
+										`relative cursor-default select-none py-2 pl-10 pr-4 ${
+											active ? "bg-amber-100 text-amber-900" : "text-gray-900"
+										}`
+									}
+									value={fileFormat}
+									disabled={fileFormat.unavailable}>
+									{({ selected }) => (
+										<>
+											<span
+												className={`block truncate ${
+													selected ? "font-medium" : "font-normal"
+												} ${fileFormat.unavailable ? `text-gray-500` : ""}`}>
+												{fileFormat.name}
+											</span>
+											{selected ? (
+												<span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600 ">
+													<CheckIcon className="h-5 w-5" aria-hidden="true" />
+												</span>
+											) : null}
+										</>
+									)}
+								</Listbox.Option>
+							))}
+						</Listbox.Options>
+					</Transition>
+				</div>
+			</Listbox>
+		</div>
+	);
 }
